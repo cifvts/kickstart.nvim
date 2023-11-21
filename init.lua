b012cb3dc3030806969677b2e1f074915b40ce8e
Andrea Agosti <aa.gh.xpxob@simplelogin.com>
2023-11-21 11:19:00 +0100

    Force using groovy filetype for Jenkinsfile

--- init.lua
+++ init.lua
@@ -1023,5 +1023,14 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   group = format_sync_grp,
 })
 
+-- [[ Filetype Detection ]]
+-- Detect Jenkinsfile as a Groovy Filetype
+vim.filetype.add {
+  filename = {
+    ['Jenkinsfile'] = 'groovy',
+    ['jenkinsfile'] = 'groovy',
+  },
+}
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
