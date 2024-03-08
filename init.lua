ee0c6495efb03a6eeddac38c1dc7eb2acbe24cfd
Andrea Agosti <aa.gh.xpxob@simplelogin.com>
2024-03-08 10:11:22 +0100

    Add plugin for Helm template yaml stuff

--- init.lua
+++ init.lua
@@ -249,6 +249,9 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
+  -- yaml helm syntax
+  'towolf/vim-helm',
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -1031,6 +1034,9 @@ vim.filetype.add {
     ['jenkinsfile'] = 'groovy',
   },
 }
+
+-- CoPilot configuration without tab
+vim.g.copilot_assume_mapped = true
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
