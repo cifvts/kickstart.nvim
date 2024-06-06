a2f48ba568db4059e538c6e1e0f185629d6f1fb8
Andrea Agosti <aa.gh.xpxob@simplelogin.com>
2024-06-06 10:14:17 +0200

    Some settings for gopls and work stuff

--- init.lua
+++ init.lua
@@ -675,7 +675,13 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        gopls = {},
+        gopls = {
+          settings = {
+            gopls = {
+              buildFlags = { '-tags=integration_dynamo integration_cassandra integration_kafka integration' },
+            },
+          },
+        },
         pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
