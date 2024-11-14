b6ce39044cf54bd8e78d4c58e60acfd046f6088b
Andrea Agosti <aa.gh.xpxob@simplelogin.com>
2024-11-14 08:21:20 +0100

    Restore quick command to see errors

--- init.lua
+++ init.lua
@@ -174,6 +174,7 @@ vim.o.confirm = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
